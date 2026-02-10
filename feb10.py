d11286ccb8f03209ea64fb0dd3000d03fe5a0e68
Sayan Debnath <sayandebnath20004@gmail.com>
2026-02-10 12:55:49 +0530

    Add files via upload

---
+++ feb10.py
@@ -0,0 +1,48 @@
+# a=[9,2,3,45,5,67,1]
+# b=a
+# c=a.copy()
+# d=c.copy()
+# b.append(11)
+# c.append(111)
+# d.append(1111)
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+ 
+# a=[[1,2],[3,4]]
+# b=a.copy()
+# b[0].append(10)
+# print(a)
+# print(b)
+
+# import copy
+# a=[[1,2],[3,4]]
+# b=copy.deepcopy(a)
+# b[0].append(10)
+# print(a)
+# print(b)
+
+# i = "I am Busy right now"
+# words = i.split()
+# r = words[::-1]
+# o = ' '.join(r)
+# print(o)
+
+# i="I am Busy right now"
+# print(' '.join(i.split()[::-1]))
+
+
+# a=[2,5,4,10,1,5,3]
+# print(a[-2:-6:-1])
+
+
+b=([2,3],[4,5,10])
+b[0].append(5)
+print(b)
+
+# b[0][0]+=1
+# print(b)
+
+# b[0]=[5,3,2]
+# print(b)
