2ac461a0d707e02e1f7b5be6b8e0b14a1b697ee0
Sayan Debnath <sayandebnath20004@gmail.com>
2026-03-31 12:48:37 +0530

    Add matrix transposition functionality

---
+++ march31st.py
@@ -0,0 +1,16 @@
+matrix = [
+    [2, 3, 4],
+    [5, 6, 7]
+]
+
+transpose = []
+
+for i in range(len(matrix[0])):
+    row = []
+    for j in range(len(matrix)):
+        row.append(matrix[j][i])
+    transpose.append(row)
+
+print("Transpose Matrix:")
+for row in transpose:
+    print(row)
