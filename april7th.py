545116800feba18941f956041aa6bab2ec8c053a
Sayan Debnath <sayandebnath20004@gmail.com>
2026-04-07 13:28:55 +0530

    Update april7th.py

--- april7th.py
+++ april7th.py
@@ -14,3 +14,15 @@ df1=pd.DataFrame(data1)
 df2=pd.DataFrame(data2)
 merge=pd.merge(df1,df2,on=["roll","name"],how="outer")
 print(merge)
+
+
+# numpy closest number
+import numpy as np
+
+numbers = [10, 220, 600, 300, 400]
+target = 26
+
+arr = np.array(numbers)
+closest = arr[np.abs(arr - target).argmin()]
+
+print("Closest number is:", closest)
