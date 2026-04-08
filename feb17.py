62428fce6ed67f7a889d64ae5ae5d40af5ac4fb5
Sayan Debnath <sayandebnath20004@gmail.com>
2026-04-08 20:33:07 +0530

    Update feb17.py

--- feb17.py
+++ feb17.py
@@ -1,18 +1,18 @@
-# def fibonacci(a):
-#     if a <= 0:
-#         return 0
-#     elif a == 1:
-#         return 0
-#     elif a == 2:
-#         return 1
-#     else:
-#         return fibonacci(a-1) + fibonacci(a-2)
+def fibonacci(a):
+    if a <= 0:
+        return 0
+    elif a == 1:
+        return 0
+    elif a == 2:
+        return 1
+    else:
+        return fibonacci(a-1) + fibonacci(a-2)
 
-# b = int(input("Enter number of terms: "))
+b = int(input("Enter number of terms: "))
 
-# print("Fibonacci Series:")
-# for i in range(1, b + 1):
-#     print(fibonacci(i),end=" ")
+print("Fibonacci Series:")
+for i in range(1, b + 1):
+    print(fibonacci(i),end=" ")
 
 
 
